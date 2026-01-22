0db8fb05fd5121fcb4e2fa2c833749df4df7f7f1
PeterSlezak <peter.slezak5@gmail.com>
2026-01-22 19:29:18 +0100

    Add files via upload

---
+++ Generated/VersionAttributes.vb
@@ -0,0 +1,5 @@
+﻿Imports System.Reflection
+<Assembly: AssemblyVersion("0.1.8.0")>
+<Assembly: AssemblyFileVersion("0.1.8.0")>
+<Assembly: AssemblyInformationalVersion("0.1.8.0")>
+<Assembly: AssemblyMetadata("BuildDate", "2026-01-21")>
